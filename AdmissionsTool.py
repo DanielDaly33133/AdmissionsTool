a68f003f184a3fc031b22116fb89698f0f440bff
DanielDaly33133 <ddaly33133@gmail.com>
2024-10-20 22:29:01 -0400

    Second Commit

--- AdmissionsTool.py
+++ AdmissionsTool.py
@@ -1,10 +1,22 @@
 #Function: This program determines if a student will be admitted or rejected.
+
 #Input:  Interactive
+
+response1 = input("PLease enter student test score: ")
+
+
+
+response2 = input("Please enter student class rank: ")
+
+
+
 #Output: Accept or Reject
-
 # Get input and convert to correct data type for testScore and classRank
-
 # Test using admission requirements and print Accept or Reject
+testScore = int(response1)
+
+classRank = int(response2)
+
 if testScore >= 90:
   if classRank >= 25:
     print("Accept")
